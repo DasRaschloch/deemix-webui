336d3dd8a84072a65d13698a83e6c82c5006c99c
RemixDev <RemixDev64@gmail.com>
2020-05-17 18:21:40 +0200

    Revert "Added easy login in the GUI"
    This reverts commit f4d01e2067fb4575455ae1230e4a1e21f3b3dd45.
    CEF doesn't support intents so this method doesn't work on Windows

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -41596,9 +41596,6 @@ const SettingsTab = new Vue({
 		logout() {
 			socket.emit('logout');
 		},
-		applogin() {
-			if (window.clientMode) socket.emit('applogin');
-		},
 		initSettings(settings, credentials, defaults) {
 			this.loadSettings(settings, credentials, defaults);
 			toast('Settings loaded!', 'settings');
@@ -42102,13 +42099,6 @@ function startApp() {
 	TrackPreview.init();
 }
 
-function initClient(){
-	console.log("ClientMode");
-	document.getElementById("settings_btn_appLogin").removeAttribute("disabled");
-	window.clientMode = true;
-}
-
-window.addEventListener('pywebviewready', initClient);
 document.addEventListener('DOMContentLoaded', startApp);
 
 /* ===== General functions ===== */
@@ -42165,7 +42155,6 @@ socket.on('logged_in', function (data) {
 				document.getElementById('logged_in_info').classList.remove('hide');
 			}
 			document.getElementById('home_not_logged_in').classList.add('hide');
-			document.getElementById('settings_btn_appLogin').classList.add('hide');
 			break
 		case 2:
 			toast('Already logged in', 'done', true, 'login-toast');
@@ -42179,7 +42168,6 @@ socket.on('logged_in', function (data) {
 				document.getElementById('logged_in_info').classList.remove('hide');
 			}
 			document.getElementById('home_not_logged_in').classList.add('hide');
-			document.getElementById('settings_btn_appLogin').classList.add('hide');
 			break
 		case 0:
 			toast("Couldn't log in", 'close', true, 'login-toast');
@@ -42191,7 +42179,6 @@ socket.on('logged_in', function (data) {
 			jquery('#settings_username').text('Not Logged');
 			jquery('#settings_picture').attr('src', `https://e-cdns-images.dzcdn.net/images/user/125x125-000000-80-0-0.jpg`);
 			document.getElementById('home_not_logged_in').classList.remove('hide');
-			document.getElementById('settings_btn_appLogin').classList.remove('hide');
 			break
 	}
 });
@@ -42205,6 +42192,5 @@ socket.on('logged_out', function () {
 	jquery('#settings_username').text('Not Logged');
 	jquery('#settings_picture').attr('src', `https://e-cdns-images.dzcdn.net/images/user/125x125-000000-80-0-0.jpg`);
 	document.getElementById('home_not_logged_in').classList.remove('hide');
-	document.getElementById('settings_btn_appLogin').classList.remove('hide');
 });
 //# sourceMappingURL=bundle.js.map

da657bbdcc1bb61938f0392f77cd170d632847df
Roberto Tonino <roberto.tonino5@gmail.com>
2020-06-03 18:31:38 +0200

    added rollup-plugin-analyzer to retrieve useful information when building

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
 import alias from '@rollup/plugin-alias'
-import { terser } from 'rollup-plugin-terser'
+import analyze from 'rollup-plugin-analyzer'
 
 // 'rollup -c' -> 'production' is false
 // 'rollup -c -w' -> 'production' is true
@@ -32,6 +33,7 @@ export default {
 		}),
 		resolve(), // Tells Rollup how to find imported modules in node_modules
 		commonjs(), // Converts imported modules to ES modules, if necessary
-		production && terser() // Minify, but only in production
+		production && terser(), // Minify, but only in production
+		production && analyze({ showExports: true }) // Show useful information about bundles, only in production
 	]
 }

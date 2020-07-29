c935a33fdbbe755a1de566a4be92acb1f1d8069b
RemixDev <RemixDev64@gmail.com>
2020-07-29 16:29:25 +0200

    Added german translation

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -5,6 +5,7 @@ import VueI18n from 'vue-i18n'
 import it from '@/lang/it'
 import en from '@/lang/en'
 import es from '@/lang/es'
+import de from '@/lang/de'
 
 Vue.use(VueI18n)
 
@@ -15,13 +16,30 @@ document.querySelector('html').setAttribute('lang', DEFAULT_LANG)
 const locales = {
 	it,
 	en,
-	es
+	es,
+	de
 }
 
 const i18n = new VueI18n({
 	locale: DEFAULT_LANG,
 	fallbackLocale: DEFAULT_LANG,
-	messages: locales
+	messages: locales,
+	pluralizationRules: {
+    /**
+     * @param choice {number} a choice index given by the input to $tc: `$tc('path.to.rule', choiceIndex)`
+     * @param choicesLength {number} an overall amount of available choices
+     * @returns a final choice index to select plural word by
+     */
+    'ru': function(choice, choicesLength) {
+      if (choice === 0) { return 0 }
+			var n = Math.abs(choice) % 100
+			var n1 = n % 10
+	    if (n > 10 && n < 20) { return 3 }
+	    if (n1 > 1 && n1 < 5) { return 2 }
+	    if (n1 == 1) { return 1 }
+	    return 3;
+    }
+  }
 })
 
 export default i18n
--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,9 +1,11 @@
 import it from 'svg-country-flags/svg/it.svg'
 import gb from 'svg-country-flags/svg/gb.svg'
 import es from 'svg-country-flags/svg/es.svg'
+import de from 'svg-country-flags/svg/de.svg'
 
 export default {
 	it,
 	en: gb,
-	es
+	es,
+	de
 }

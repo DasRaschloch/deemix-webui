d20540f46ed0a8785a526a56bc79217f5d1ffdcd
Roberto Tonino <roberto.tonino5@gmail.com>
2020-11-20 17:31:12 +0100

    feat: label to distinguish release type in All tab in Artist page; build: v1.3.6

--- src/components/pages/Artist.vue
+++ src/components/pages/Artist.vue
@@ -56,17 +56,19 @@
 						<img class="mr-4 rounded coverart" :src="release.releaseCover" style="width: 56px; height: 56px" />
 						<i v-if="release.isReleaseExplicit" class="material-icons title-icon title-icon--explicit">explicit</i>
 						<div>
-							<span class="flex hover:text-primary">{{ release.releaseTitle }}</span>
-							<!-- <span v-show="currentTab === 'all'" class="flex text-xs opacity-50 uppercase-first-letter">
-								{{ release.releaseType }}
-							</span> -->
+							<span class="flex hover:text-primary">
+								{{ release.releaseTitle }}
+								<i
+									v-if="checkNewRelease(release.releaseDate)"
+									class="material-icons title-icon title-icon--right title-icon--new"
+								>
+									fiber_new
+								</i>
+							</span>
+							<span v-show="currentTab === 'all'" class="block text-xs opacity-50 uppercase-first-letter">
+								{{ $tc(`globals.listTabs.${release.releaseType}`) }}
+							</span>
 						</div>
-						<i
-							v-if="checkNewRelease(release.releaseDate)"
-							class="material-icons title-icon title-icon--right title-icon--new"
-						>
-							fiber_new
-						</i>
 					</RouterLink>
 					<td class="w-32 text-center xl:w-40">{{ release.releaseDate }}</td>
 					<td class="w-20 text-center xl:w-32">{{ release.releaseTracksNumber }}</td>
--- src/data/artist.js
+++ src/data/artist.js
@@ -25,7 +25,7 @@ function formatArtistReleases(artistReleases) {
 					releaseDate: getPropertyWithFallback(release, 'release_date'),
 					releaseTracksNumber: getPropertyWithFallback(release, 'nb_tracks'),
 					releaseLink: getPropertyWithFallback(release, 'link'),
-					releaseType: getPropertyWithFallback(release, 'type'),
+					releaseType: getPropertyWithFallback(release, 'record_type'),
 					isReleaseExplicit: getPropertyWithFallback(release, 'explicit_lyrics')
 				})
 			}

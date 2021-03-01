cbf3a5c6776a0e4f0cab516d48024f396d0d9983
Roberto Tonino <roberto.tonino5@gmail.com>
2021-03-01 21:22:32 +0100

    fix: removed multiple message events added to CustomWebsocket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,21 +1,31 @@
 import store from '@/store'
 
+let wasEventListenerAdded = false
+
 class CustomSocket extends WebSocket {
-  constructor(args) {
-    super(args)
-  }
-
-	emit(key, data) {
-		if (this.readyState != WebSocket.OPEN) return false
-		this.send(JSON.stringify({key:key, data:data}))
+	constructor(args) {
+		super(args)
 	}
+	emit(key, data) {
+		if (this.readyState !== WebSocket.OPEN) return false
+		this.send(JSON.stringify({ key: key, data: data }))
+	}
+	on(key, cb) {
+		if (!wasEventListenerAdded) {
+			wasEventListenerAdded = true
 
-	on(key, callback) {
-		this.addEventListener('message', function(event){
-			let data = JSON.parse(event.data)
-      console.log(data)
-			if (data.key == key) callback(data.data)
-		})
+			this.addEventListener('message', event => {
+				const messageData = JSON.parse(event.data)
+
+				if (messageData.key === key) {
+					cb(messageData.data)
+				}
+			})
+		}
+	}
+	off() {
+		console.log('off!')
+		// this.removeEventListener('message')
 	}
 }
 
